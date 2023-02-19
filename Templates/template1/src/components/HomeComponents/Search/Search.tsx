21bc5f3e40b4baf142c4e4d51025ede6bf25b97b
SrLuc <ruanlucenaramos@gmail.com>
2023-02-19 13:48:32 -0300

    estilizando o botao de pesquisa

--- Templates/template1/src/components/HomeComponents/Search/Search.tsx
+++ Templates/template1/src/components/HomeComponents/Search/Search.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import * as S from "./style";
 
+//icons
+import { RiListSettingsLine } from "react-icons/ri";
+
 interface SearchProps {}
 
 const Search = ({}: SearchProps) => {
   return (
     <S.StyledSearch>
       <input type="search" placeholder="Search.." />
-      <button type="submit">reactIcon</button>
+      <button type="submit">
+        {<RiListSettingsLine size={25} color="#fff" />}
+      </button>
     </S.StyledSearch>
   );
 };
--- Templates/template1/src/components/HomeComponents/Search/style.ts
+++ Templates/template1/src/components/HomeComponents/Search/style.ts
@@ -5,16 +5,18 @@ export const StyledSearch = styled.div`
   justify-content: space-between;
   padding: 15px;
   margin: 5px;
-  border: 1px solid red;
 
   input {
     padding: 2px 20px;
     width: 300px;
     height: 44px;
-    background: #f5f5f5;
     border-radius: 16px;
     margin-right: 5px;
     border: none;
+    gap: 8px;
+    background-color: #f5f5f5;
+    border-radius: 16px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 15px 60px -12px, rgba(0, 0, 0, 0.3) 0px 1px 36px -18px inset;
   }
 
   button {
