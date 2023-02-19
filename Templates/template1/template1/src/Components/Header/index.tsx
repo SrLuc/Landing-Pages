f69e4ddd3610f6af49847f5c54517e51a68939af
SrLuc <ruanlucenaramos@gmail.com>
2023-02-18 23:46:01 -0300

    criando header da aplicacao mobile

--- Templates/template1/template1/src/Components/Header/index.tsx
+++ Templates/template1/template1/src/Components/Header/index.tsx
@@ -9,7 +9,7 @@ const Header = ({}: HeaderProps) => {
   return (
     <S.StyledHeader>
       <img src={logo} alt="Logo" />
-      <h4>Welcome back, Pin! How Hungry are You</h4>
+      <h4>Welcome back, Pin! How Hungry are You ?</h4>
     </S.StyledHeader>
   );
 };
