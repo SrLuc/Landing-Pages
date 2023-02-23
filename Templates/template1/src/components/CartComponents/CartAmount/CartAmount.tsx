4585ee25e5104d09f279081cce0728092b4d95db
SrLuc <ruanlucenaramos@gmail.com>
2023-02-22 22:44:45 -0300

    funcao para add produtos e multiplicar o valor final, porem precisa ser refatorada pois esta dando bugs

--- Templates/template1/src/components/CartComponents/CartAmount/CartAmount.tsx
+++ Templates/template1/src/components/CartComponents/CartAmount/CartAmount.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./styles";
 import { MdOutlineAdd } from "react-icons/md";
 import { IoIosRemove } from "react-icons/io";
@@ -6,20 +6,42 @@ import { IoIosRemove } from "react-icons/io";
 interface CartAmountProps {}
 
 const CartAmount = ({}: CartAmountProps) => {
+  const [count, setCount] = useState(1);
+  const [finalPrice, setFinalPrince] = useState(10.98);
+
+
+//Consetar Função com as boas praticas do hook useState  
+//   function removeAmount() {
+//     setCount(count - 1);
+//     setFinalPrince(finalPrice - 10.98);
+//   }
+
+//   function addAmount() {
+//     setCount(count + 1);
+//     setFinalPrince(finalPrice + 10.98);
+//   }
+
   return (
     <S.StyledCartAmount>
       <div className="div1">
-        <i className="then">{<IoIosRemove size={30} color="#3d405b" />}</i>
-        <p>0</p>    
-        <i className="more">{<MdOutlineAdd size={30} color="#fff"/>}</i>
+        <i className="then" id="menos" onClick={() => removeAmount()}>
+          {<IoIosRemove size={30} color="#3d405b" />}
+        </i>
+        <p>{count}</p>
+        <i className="more" id="mais" onClick={() => addAmount()}>
+          {<MdOutlineAdd size={30} color="#fff" />}
+        </i>
       </div>
 
       <div className="div2">
         <h4>Add to Cart</h4>
-        <h2>$27.45</h2>
+        <h2>{finalPrice}</h2>
       </div>
     </S.StyledCartAmount>
   );
 };
 
 export default CartAmount;
+
+//onClick={() => setCount(count - 1)}
+//onClick={() => setCount(count + 1)}
